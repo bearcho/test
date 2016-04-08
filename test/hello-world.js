7487eaf14ac17a2a632b9002832760dbe7d37be0
peter <peter@bearcho-PC>
2016-04-08 17:25:14 +0900

    테스ㅡ

--- test/hello-world.js
+++ test/hello-world.js
@@ -1,10 +1,43 @@
-var http = require('http');
+//var http = require('http');
+//
+//http.createServer(function hanler(req, res) {
+//	res.writeHead(200, {'Conent-Type' : 'text/plain'});
+//	res.end('HELLO WORLD 2');
+//	
+//}).listen(1337 , '127.0.0.1');
+//
+//console.log('server running at http://127.0.0.1:1337'); 
 
-http.createServer(function hanler(req, res) {
-	res.writeHead(200, {'Conent-Type' : 'text/plain'});
-	res.end('HELLO WORLD 2');
+//http.request
+
+var request = require('request');
+var iconv = require('iconv-lite');
+//var request = require('inconv').Iconv;
+
+
+
+
+//var request = require('request');
+
+
+//request.setEncoding("euckr");
+request.get({url: 'http://cj.cj.net/PT/login.aspx' , encoding: null}, function(err, response, body){
+//request.get({url: 'http://naver.com' , encoding: 'binary'}, function(err, response, body){
+//request.get({url: 'http://www.kongju.ac.kr/lounge/board.jsp?board=student_news&page=0' , encoding: null}, function(err, response, body){
+//	buf = new Iconv(body, 'euckr');
+//	var buf    = new Buffer(body,'binary');
+	iconv.extendNodeEncodings();
 	
-}).listen(1337 , '127.0.0.1');
+//	var str = iconv.decode(new Buffer(body), "euc-kr");
 
-console.log('server running at http://127.0.0.1:1337'); 
-
+	
+	
+	
+	
+	
+//	console.log(iconv.decode(buf.toString(),'euc-kr' ));
+	console.log(body.toString());
+	
+//    console.log(buf.toString('euckr'));
+})
+ 
